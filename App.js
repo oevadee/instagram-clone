f5e80387c01883879cd873a859c5e6f3f6b2bcb6
Adrian <62460030+oevadee@users.noreply.github.com>
2020-12-08 23:06:25 +0100

    added bottom tab bar navigation with icons. Rn there is no sollution to add -outline icons.

--- App.js
+++ App.js
@@ -1,108 +1,68 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, Button, SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import "react-native-gesture-handler";
-import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import Entypo from "react-native-vector-icons/Entypo";
+import {
+  ExploreScreen,
+  HomeScreen,
+  AddImage,
+  ProfileScreen,
+  StoreScreen,
+} from "./screens/mainStackScreens";
+import {
+  SafeAreaProvider,
+  initialWindowMetrics,
+} from "react-native-safe-area-context";
 
-const HomeScreen = ({ navigation }) => {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Explore"
-        onPress={() => {
-          navigation.navigate("Explore");
-        }}
-      />
-    </View>
-  );
-};
-
-const ExploreScreen = ({ navigation }) => {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Explore Screen</Text>
-      <Button
-        title="Home"
-        onPress={() => {
-          navigation.navigate("Home");
-        }}
-      />
-    </View>
-  );
-};
-
-const StoreScreen = ({ navigation }) => {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Store Screen</Text>
-      <Button
-        title="Home"
-        onPress={() => {
-          navigation.navigate("Home");
-        }}
-      />
-    </View>
-  );
-};
-
-const ProfileScreen = ({ navigation }) => {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Profile Screen</Text>
-      <Button
-        title="Home"
-        onPress={() => {
-          navigation.navigate("Home");
-        }}
-      />
-    </View>
-  );
-};
-
-// const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const app = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size = 32 }) => {
+              let iconName;
 
-            if (route.name === "Home") {
-              iconName = focused
-                ? "ios-information-circle"
-                : "ios-information-circle-outline";
-            } else if (route.name === "Explore") {
-              iconName = focused ? "ios-list-box" : "ios-list";
-            } else if (route.name === "Store") {
-              iconName = focused ? "ios-list-box" : "ios-list";
-            } else if (route.name === "Profile") {
-              iconName = focused ? "ios-list-box" : "ios-list";
-            }
+              if (route.name === "Home") {
+                iconName = focused ? "ios-home" : "ios-home";
+              } else if (route.name === "Explore") {
+                iconName = focused ? "ios-search" : "ios-search";
+              } else if (route.name === "AddImage") {
+                iconName = focused ? "ios-add" : "ios-add";
+                size = 48;
+              } else if (route.name === "Store") {
+                iconName = focused ? "ios-cart" : "ios-cart";
+              } else if (route.name === "Profile") {
+                iconName = focused ? "ios-person" : "ios-person";
+              }
 
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: "#fcfcfc",
-          inactiveTintColor: "#dfdfdf",
-          inactiveBackgroundColor: "#121212",
-          activeBackgroundColor: "#121212",
-        }}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Explore" component={ExploreScreen} />
-        <Tab.Screen name="Store" component={StoreScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: "#f5f5f5",
+            inactiveTintColor: "#d5d5d5",
+            style: {
+              backgroundColor: "#121212",
+              borderTopColor: "transparent",
+            },
+            showLabel: false
+          }}
+        >
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Explore" component={ExploreScreen} />
+          <Tab.Screen name="AddImage" component={AddImage} />
+          <Tab.Screen name="Store" component={StoreScreen} />
+          <Tab.Screen name="Profile" component={ProfileScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
---
+++ screens/mainStackScreens.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { StyleSheet, Text, View, Button, SafeAreaView } from "react-native";
+
+export const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={[{ flex: 1, alignItems: "center", justifyContent: "center" }, styles.body]}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Explore"
+        onPress={() => {
+          navigation.navigate("Explore");
+        }}
+      />
+    </View>
+  );
+};
+
+export const ExploreScreen = ({ navigation }) => {
+  return (
+    <View style={[{ flex: 1, alignItems: "center", justifyContent: "center" }, styles.body]}>
+      <Text>Explore Screen</Text>
+      <Button
+        title="Home"
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      />
+    </View>
+  );
+};
+
+export const AddImage = ({ navigation }) => {
+  return (
+    <View style={[{ flex: 1, alignItems: "center", justifyContent: "center" }, styles.body]}>
+      <Text>Add Image</Text>
+      <Button
+        title="Home"
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      />
+    </View>
+  );
+};
+
+export const StoreScreen = ({ navigation }) => {
+  return (
+    <View style={[{ flex: 1, alignItems: "center", justifyContent: "center" }, styles.body]}>
+      <Text>Store Screen</Text>
+      <Button
+        title="Home"
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      />
+    </View>
+  );
+};
+
+export const ProfileScreen = ({ navigation }) => {
+  return (
+    <View style={[{ flex: 1, alignItems: "center", justifyContent: "center" }, styles.body]}>
+      <Text>Profile Screen</Text>
+      <Button
+        title="Home"
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: '#121212'
+  }
+})
